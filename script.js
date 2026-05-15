54c2a5a954833813dc24f520c8643ab57dde67cb
Priyankasapp <priyanka.s@applocum.org>
2026-05-15 15:21:19 +0530

    new changes added on sidebar module ...!

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     var menuIcon = document.querySelector('.menu-icon');
     var sidebar = document.querySelector('.sidebar');
+    var container = document.querySelector('.container');
 
     menuIcon.addEventListener("click", function(){
         sidebar.classList.toggle("small-sidebar");
+        container.classList.toggle("large-container");
     });
 });
